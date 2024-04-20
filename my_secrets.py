a64de0f32d532732df5b40b67e747da0d45c36d9
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-04-21 01:15:07 +0300

    Delete my_secrets.py

--- my_secrets.py
+++
@@ -1,4 +0,0 @@
-CHATGPT_API_KEY = "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"
-DB_URL = "mongodb+srv://makinci473:571632@cluster0.fja9ahf.mongodb.net/"
-#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 önceki api url (çalışmıyor)
-# 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 diğer api url (çalışmıyor)
