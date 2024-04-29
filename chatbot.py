08f686fc84e50af502b2e0cdf0210e19772d4abe
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-04-29 21:03:49 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,28 +13,16 @@ except Exception as e:
 
 # Konu başlıkları ve cevapları
 topic_responses = {
-    "blockchain": "",
-    "yapay zeka": "",
-    "yazılım": "",
-    "makine öğrenmesi": "",
-    "derin öğrenme": "",
-    "software": "",
-    "blokzincir": "",
-    "zero knowledge": "",
-    "artificial intelligence": "", 
-    "machine learning": "",
-    "deep learning": "", 
-    "mina protocol": "",
-    "launchpad": "", 
-    "kripto": "", 
-    "crypto":"",
-    "sıfır bilgi ispatı": "", 
-    "coin": "",
-    "ethereum": "", 
-    "solidity":"", 
-    "evm":"", 
-    "smart contract": "", 
-    "akıllı kontrat": "",
+    "blockchain": "", "blokzincir": "",
+    "yazılım": "", "software": "",
+    "zero knowledge": "", "sıfır bilgi ispatı": "", 
+    "artificial intelligence": "", "yapay zeka": "", "machine learning": "", "deep learning": "", "derin öğrenme": "", "makine öğrenmesi": "",
+    "mina protocol": "", "launchpad": "", 
+    "kripto": "", "crypto":"", "coin": "",
+    "ethereum": "", "solidity":"", "evm":"", 
+    "smart contract": "", "akıllı kontrat": "",
+    "merhaba": "","greetings": "", 
+    "hi": "", "hello": "",
 }
 
 def get_response(message):
@@ -58,6 +46,7 @@ def get_response(message):
 
 def kaydetSoruCevap(soru, cevap):
     try:
+        print("Please note, while the Atlaspad Ai aims to be helpful, it is currently in its beta phase. We encourage you to explore its capabilities, but remind you that it may not always provide perfect answers. Your feedback during this beta phase is invaluable and will help us improve its accuracy and functionality. Enjoy the conversation and let us know your thoughts!")
         # Soru ve cevapları veritabanına kaydedin
         sorular_collection = db['sorular']  # Sorular koleksiyonunu kullanın
         cevaplar_collection = db['cevaplar']  # Cevaplar koleksiyonunu kullanın
@@ -74,14 +63,22 @@ def chatCevapAl(message):
         if "atlaspad" in message.lower():
             if "atlaspad nedir" in message.lower():
                 return "Atlaspad, Mina Protocol'da kullanılan ZK-Launchpad teknolojisidir. Birden fazla blok zincirinde güvenli ve özel işlemlere olanak sağlamak için sıfır bilgi kanıtlarını kullanan ilk ve tek güvenilir Çapraz Zincir ZK Launchpad'i tanıtıyor. Bu son teknoloji platform, kullanıcıların şunları yapmasına olanak tanıyarak DeFi'deki acil gizlilik sorunlarına ve yüksek işlem maliyetlerine çözüm getiriyor."
+            elif "what is atlaspad" in message.lower():
+                return "Atlaspad is the ZK-Launchpad technology used in Mina Protocol. Introducing ZK Launchpad, the first and only trusted Cross-Chain that uses zero-knowledge proofs to enable secure and private transactions across multiple blockchains. This cutting-edge platform addresses pressing privacy issues and high transaction costs in DeFi by allowing users to."
             elif "atlaspad neden tasarlandı" in message.lower():
                 return "Bu başlatma paneli, kullanıcılara varlıkları zincirler arasında gizli ve verimli bir şekilde taşıma özgürlüğünü sağlamak ve gizliliğin ve zincirler arası işlevselliğin sorunsuz bir şekilde bir arada var olduğu DeFi alanında yeni bir paradigmayı teşvik etmek için tasarlandı."
+            elif "why was atlaspad designed" in message.lower():
+                return "This launchpad is designed to provide users with the freedom to move assets between chains privately and efficiently, fostering a new paradigm in the DeFi space where privacy and cross-chain functionality seamlessly coexist."
             elif "atlaspad hakkında bilgi ver" in message.lower():
                 return "Günümüzün kripto para ekosisteminde, yatırımcılar arasında fırlatma rampalarının kullanımı giderek yaygınlaşarak çapraz ölçekli kripto varlık işlemlerini kolaylaştırıyor. Başlatma rampaları genellikle tek bir ağ tarafından desteklenen merkezi veya merkezi olmayan yapılardır. Kripto-ekonomik üçlemeye göre, eğer bir fırlatma rampası merkezileştirilirse güvenlik pahasına hız kazanabilir; merkezden dağıtılmışsa güvenilirlik sunar ancak işlem hızını olumsuz yönde etkileyebilir. Herhangi bir fırlatma rampası ve kullanıcıları, özellikle de yatırımcılar için temel sorunlar, güvenlik ve gizlilik etrafında dönüyor. Doğrulanmamış işlemler için tercih edilmelerine rağmen fırlatma rampaları, kripto finans sektöründe suiistimallere maruz kalıyor. Ayrıca bunlar doğrulandığında orantısız merkezileşme ve veri güvenliğinin istismar edilmesi gibi sorunlar ortaya çıkıyor. Üstelik MINA ağında herhangi bir zk-Launchpad'in bulunmaması ve MINA'nın auro cüzdanı dışında EVM uyumluluğunun stabil token eksikliği nedeniyle daha az tercih edilmesi, bizi tüm bu sorunları çözecek karar almaya yöneltti. . Bu sorunları geliştirici ekibimiz, deneyimli kadromuz ve tabii ki (zkproof) ile ele alarak, gerçek anlamda çözüm olarak gördüğümüz MINA ağına kullanıcı katılımını arttırırken, tamamen doğrulanabilen ve anonim olabilen bir sistem kurmayı hedefliyoruz.  Atlaspad bir projeden daha fazlasıdır; gizliliğin ölçeklenebilirlik ve birlikte çalışabilirlikle buluştuğu, DeFi'nin geleceğine adanmış, büyüyen bir ekosistemdir. Yolculuğumuz sürekli yenilik, işbirlikçi gelişim ve topluluğumuzun sarsılmaz desteğiyle güçleniyor."
-            elif "teşekkürler" in message.lowe():
+            elif "give information about atlaspad" in message.lower():
+                return "In today's cryptocurrency ecosystem, the use of launch pads is becoming increasingly common among investors, facilitating cross-scale crypto asset transactions. Launchpads are generally centralized or decentralized structures supported by a single network. According to the crypto-economic trilogy, if a launchpad is centralized it can gain momentum at the expense of security; If decentralized, it offers reliability but may negatively impact transaction speed. The key issues for any launchpad and its users, especially investors, revolve around security and privacy. Although preferred for unverified transactions, launchpads are subject to abuses in the crypto finance industry. Additionally, when these are verified, problems such as disproportionate centralization and abuse of data security arise. Moreover, the absence of any zk-Launchpad on the MINA network and the fact that EVM compatibility other than MINA's auro wallet is less preferred due to the lack of stable tokens, led us to make a decision to solve all these problems. . By addressing these problems with our developer team, experienced staff and of course (zkproof), we aim to establish a system that can be fully verified and anonymous while increasing user participation in the MINA network, which we see as a real solution. Atlaspad is more than a project; is a growing ecosystem dedicated to the future of DeFi, where privacy meets scalability and interoperability. Our journey is fueled by continuous innovation, collaborative development, and the unwavering support of our community."
+            elif "teşekkürler" in message.lower():
                 return "Rica ederim. Her zaman buradayım..."
+            elif "thanks" in message.lower():
+                return "You're welcome I'm always here..."
             else:
-                return "Atlaspad hakkında ne sormak istediğinizi belirtir misiniz?"
+                return "Could you please specify what you want to ask about Atlaspad?"
             
 
         # Diğer belirli içeriklere cevap ver
